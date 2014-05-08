cc190297c8b3e4f93821feb7655add07854745ff
Reimer Behrends <behrends@gmail.com>
2014-05-08 12:05:33 +0200

    Make local include/lib directory available by default.

--- build-darwin.sh
+++ build-darwin.sh
@@ -1,4 +1,4 @@
 SING=$1
 shift
-g++ -I$SING -I$SING/factory/include -I$SING/libpolys -O2 -c zmq.cc -DOM_NDEBUG -DNDEBUG -fPIC -DPIC -o zmq.o && \
-libtool -dynamic -twolevel_namespace -weak_reference_mismatches weak -undefined dynamic_lookup -o zmq_core.so zmq.o -lzmq "$@"
+g++ -Iinclude -I$SING -I$SING/factory/include -I$SING/libpolys -O2 -c zmq.cc -DOM_NDEBUG -DNDEBUG -fPIC -DPIC -o zmq.o && \
+libtool -dynamic -twolevel_namespace -weak_reference_mismatches weak -undefined dynamic_lookup -o zmq_core.so zmq.o -Llib -lzmq "$@"
